7c49ad1d1cdd18babc1534799b1a15ffc80dee1c
Braden Licastro <braden@fullforceapps.com>
2012-10-25 01:43:05 -0400

    Added more code t othe reference program

--- QuestionD.java
+++ QuestionD.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 public class QuestionD{
 
-    public static int start = 2;
-    public int min;
-    public int max;
+	public static int start = 2;
+	public int min;
+	public int max;
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
+		while(args[0] != 0 && args[1] ! = 0) {
+			min = args[0];
+			max = args[1];
 
-    while(args[0] != 0 && args[1] ! = 0) {
-        min = args[0];
-        max = args[1];
+		for(; start < max; start++){
+			
+		}
+	}
 
-    for(; start < max; start++){
-        
-    }
-}
-
-public int sum (int input){
-    int rsum = input;
-    while(){
-        }
+	public int sum (int input){
+		int rsum = 0;
+		for(int i = 0; i <= input; i++){
+			rsum += i;
+		}
+		return rsum;
+	}
 }
--- test.java
+++ test.java
@@ -1,20 +1,47 @@
 import java.util.*;
+import static java.lang.System.out;
 
 public class test{
 
-    public static void main(String[] args) {
-        
-        test test = new test();
-
-        System.out.println("Summation of " + args[0] + " is " + test.sum(Integer.parseInt(args[0])));
-    }
-
-	public int sum (int input){
-	    int rsum = 0;
-	    	for(int i = 0; i <= input; i++){
-                rsum += i;
-            }
-        return rsum;
+	public static int ZERO = 0;
+	public static Scanner reader = new Scanner(System.in);
+	public static Random rnd = new Random();
+	
+	public static void main(String[] args) {
+		test ts = new test();
+		
+		println("Summation of " + args[0] + " is " + ts.sum(Integer.parseInt(args[0])));
+		
+		print("The random number of the day is: " + (rnd.nextInt(9)+1) + "\n\n");
+		
+		outsideTest ot = new outsideTest();
+		
+		while(true){
+			print("What number do you want to check for a perfect square? ");
+			if(ot.isPerfectSquare(reader.nextLong()) == true){
+				println("IT IS!");
+			} else {
+				println("Nope, not a chance.");		
+			}
+		}
 	}
 
+	public double sum (int input){
+		
+		int rsum = ZERO;
+		for(int i = ZERO; i <= input; i++){
+			rsum += i;
+			
+		}
+		return (double)rsum;
+	}
+	
+	
+	public static void print(Object o){
+		out.print(o);
+	}
+	
+	public static void println(Object o){
+		out.println(o);
+	}
 }
