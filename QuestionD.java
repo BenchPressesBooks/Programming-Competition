e30a77bbae40afc686882c108b3ac82869f27d16
Braden Licastro <blicastro@live.com>
2012-10-25 18:53:27 -0400

    Almost have it

--- QuestionD.java
+++ QuestionD.java
@@ -1,43 +1,74 @@
 import java.util.*;
+import static java.lang.System.out;
 
 public class QuestionD{
 
-	public static int start = 2;
-	public int numElementsRack = 3; // number of elements in the rack (not including cue)
-	public int solutionCounter = ZERO; // number of viable solutions
-	public int min;
-	public int max;
+	public int solutionCounter = 0; // number of viable solutions
+	public int cases = 1;
+	private int min;
+	private int max;
 
 	public static void main(String[] args) {
-		while(args[0] != 0 && args[1] ! = 0) {
-			min = args[0];
-			max = args[1];
-
-		for(; start < max; start++){
+	
+		QuestionD qd = new QuestionD();
+		Scanner r = new Scanner(System.in);
+		
+		r.useDelimiter("\\s+");
+		
+		while(r.hasNextLine()) {
+		
+			String input = r.nextLine();
+			String[] elements = input.split(" ");
 			
+			qd.min = Integer.parseInt(elements[0]);
+			qd.max = Integer.parseInt(elements[1]);
+					println(qd.min);
+		println(qd.max);
+			
+			if(qd.min == 0 && qd.max == 0){
+				break;
+			} else {
+				if(qd.cases != 1)
+					print("\n");
+
+				qd.numElementsAdded(qd.min);
+						println(qd.min);
+		println(qd.max);
+				print("Case " + qd.cases + ": " + qd.solutionCounter);
+				qd.solutionCounter = 0;
+				qd.cases++;
+			}
 		}
 	}
 	
 	// Helper function to get the number of elements to be added to the correct amount
-	public int numElementsAdded(int lowerBound) {
+	public void numElementsAdded(int lowerBound) {
 		int rackSize = 1; // sets initial rack size to be just one ball
 		int numElementsAdded = 2; // initial number of elements to be added
+		QuestionD qd = new QuestionD();
+		
+		println(qd.min);
+		println(qd.max);
+		println(numElementsAdded);
 
 		while((numElementsAdded + rackSize) < lowerBound) {
 			rackSize += numElementsAdded;
 			numElementsAdded++;
 		}
-		return numElementsAdded;
+		println(qd.min);
+		println(qd.max);
+		println(numElementsAdded);
+		
+		poolSquare(qd.min, qd.max, numElementsAdded);
 	}
 	
 	// Function that computes all of the solutions
 	public int poolSquare(int lowerBound, int upperBound, int numElementsAdded) {
-		int lowerBound = args[0];
-		int upperBound = args[1];
+		QuestionD qd = new QuestionD();
 		
-		for(int (numElementsRack + 1) = lowerBound; (numElementsRack + 1) < upperBound; numElementsAdded++) {
-			if((numElementsRack + 1).isSquareNumber()) {
-				solutionCounter++;
+		for(int numElementsRack = lowerBound + 1; numElementsRack < upperBound; numElementsAdded++) {
+			if(isPerfectSquare(numElementsRack + 1)) {
+				qd.solutionCounter++;
 				numElementsRack += numElementsAdded;
 			}
 			else {
@@ -45,16 +76,7 @@ public class QuestionD{
 			}
 		}
 		
-		return solutionCounter;
-	}
-
-	// Summation of a given number
-	public int summation (int n){
-		int rsum = 0;
-		for(int i = 0; i <= n; i++){
-			rsum += i;
-		}
-		return rsum;
+		return qd.solutionCounter;
 	}
 	
 	// Checks if a given number is a perfect square.
@@ -64,5 +86,13 @@ public class QuestionD{
 		}
   		long tst = (long)(Math.sqrt(n));
   		return tst*tst == n;
+	}
+	
+	public static void print(Object o){
+		out.print(o);
+	}
+	
+	public static void println(Object o){
+		out.println(o);
 	}	
 }
--- refLib/objects.java
+++ refLib/objects.java
@@ -2,8 +2,25 @@ import java.util.*;
 
 public class objects{
 
-	public final static boolean isPerfectSquare(long n)
-	{		
+	public int min;
+	public int max;
 
+	public static void main(String[] args) {
+		Scanner r = new Scanner(System.in);
+		objects o = new objects();
+		
+		r.useDelimiter("\\s+");
+		
+		while(r.hasNextLine()) {
+	
+			String input = r.nextLine();
+			String[] elements = input.split(" ");
+			
+			o.min = Integer.parseInt(elements[0]);
+			o.max = Integer.parseInt(elements[1]);
+			
+			System.out.println(o.min + " " + o.max);
+			System.out.print("\n");
+		}
 	}
 }
